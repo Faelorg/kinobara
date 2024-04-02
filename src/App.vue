84451768825a5f2adb41c14693af20a566e0f742
Faelorg <efremovdanil1981@gmail.com>
2024-04-02 21:31:54 +0500

    First

--- src/App.vue
+++ src/App.vue
@@ -1,140 +1,149 @@
 <script lang="ts">
-import Main from './components/Main.vue';
-import { IState, IFilms } from './Types';
+import Main from './components/Main.vue'
+import { IState, IFilms, ISeances } from './Types'
 
-export default{
-  components:{
-    Main
-  },
+export default {
+	components: {
+		Main,
+	},
 
-  created(){
-    let states:IState[]=[
-      {id:1,text:"В прокате"},
-      {id:2,text:"Скоро в прокате"},
-      {id:3,text:"Выйдет в 2025"},
-      {id:4,text:"Выйдет в 2027"},
-      {id:5,text:"Выйдет в 2026"},
-    ]
-    
-    let films:IFilms[]=[
-    {
-      id:1,
-      age:18,
-      author:"Автор 1",
-      country:"США",
-      description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, et.",
-      idState:1,
-      name:"Кино 1",
-      type:"Хоррор",
-      image:"src/img/1.png"
-    },
-    
-    {
-      id:2,
-      age:6,
-      author:"Автор 2",
-      country:"Россия",
-      description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, et.",
-      idState:1,
-      name:"Кино 2",
-      type:"Мультфильм",
-      image:'src/img/2.png'
-    },
-    
-    {
-      id:3,
-      age:16,
-      author:"Автор 3",
-      country:"Великобритания",
-      description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, et.",
-      idState:1,
-      name:"Кино 3",
-      type:"Хоррор",
-      image:'src/img/3.png'
-    },
-    
-    {
-      id:4,
-      age:18,
-      author:"Автор 4",
-      country:"США",
-      description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, et.",
-      idState:2,
-      name:"Кино 4",
-      type:"Хоррор",
-      image:"../img/1.png"
-    },
-    
-    {
-      id:5,
-      age:6,
-      author:"Автор 5",
-      country:"Россия",
-      description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, et.",
-      idState:3,
-      name:"Кино 5",
-      type:"Мультфильм",
-      image:'../img/2.png'
-    },
-    
-    {
-      id:6,
-      age:16,
-      author:"Автор 6",
-      country:"Великобритания",
-      description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, et.",
-      idState:4,
-      name:"Кино 3",
-      type:"Хоррор",
-      image:'../img/2.png'
-    },
-    
-    ]
+	created() {
+		let states: IState[] = [
+			{ id: 1, text: 'В прокате' },
+			{ id: 2, text: 'Скоро в прокате' },
+			{ id: 3, text: 'Выйдет в 2025' },
+			{ id: 4, text: 'Выйдет в 2027' },
+			{ id: 5, text: 'Выйдет в 2026' },
+		]
 
-    this.$cookies.set("states",states)
+		let films: IFilms[] = [
+			{
+				id: 1,
+				age: 18,
+				author: 'Автор 1',
+				country: 'США',
+				description:
+					'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, et.',
+				idState: 1,
+				name: 'Кино 1',
+				type: 'Хоррор',
+				image: 'src/img/1.png',
+			},
 
-    this.$cookies.set("films", films)
-    }
+			{
+				id: 2,
+				age: 6,
+				author: 'Автор 2',
+				country: 'Россия',
+				description:
+					'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, et.',
+				idState: 1,
+				name: 'Кино 2',
+				type: 'Мультфильм',
+				image: 'src/img/2.png',
+			},
 
-  }
+			{
+				id: 3,
+				age: 16,
+				author: 'Автор 3',
+				country: 'Великобритания',
+				description:
+					'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, et.',
+				idState: 1,
+				name: 'Кино 3',
+				type: 'Хоррор',
+				image: 'src/img/3.png',
+			},
 
+			{
+				id: 4,
+				age: 18,
+				author: 'Автор 4',
+				country: 'США',
+				description:
+					'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, et.',
+				idState: 2,
+				name: 'Кино 4',
+				type: 'Хоррор',
+				image: '../img/1.png',
+			},
+
+			{
+				id: 5,
+				age: 6,
+				author: 'Автор 5',
+				country: 'Россия',
+				description:
+					'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, et.',
+				idState: 3,
+				name: 'Кино 5',
+				type: 'Мультфильм',
+				image: '../img/2.png',
+			},
+
+			{
+				id: 6,
+				age: 16,
+				author: 'Автор 6',
+				country: 'Великобритания',
+				description:
+					'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, et.',
+				idState: 4,
+				name: 'Кино 3',
+				type: 'Хоррор',
+				image: '../img/2.png',
+			},
+		]
+
+		let seances: ISeances[] = [
+			{ id: 1, time: '22:40', idHall: 1, idFilm: 1 },
+			{ id: 2, time: '22:45', idHall: 1, idFilm: 2 },
+			{ id: 2, time: '22:50', idHall: 1, idFilm: 3 },
+		]
+
+		this.$cookies.set('states', states)
+
+		this.$cookies.set('films', films)
+
+		this.$cookies.set('seances', seances)
+	},
+}
 </script>
 
 <template>
- <header >
-  <div class="header">
-    <div class="mainDiv">
-      <router-link to="/main" class="main">КиноБара</router-link>
-    </div>
-    <nav>
-      <router-link to="#">Акции</router-link>
-      <router-link to="#">Скоро выходит</router-link>
-      <router-link to="#">Контакты</router-link>
-    </nav>
-  </div>
+	<header>
+		<div class="header">
+			<div class="mainDiv">
+				<router-link to="" class="main">КиноБара</router-link>
+			</div>
+			<nav>
+				<router-link to="#">Акции</router-link>
+				<router-link to="#">Скоро выходит</router-link>
+				<router-link to="#">Контакты</router-link>
+			</nav>
+		</div>
 
-  <div class="seans">
-  </div> 
- </header>
- 
- 
- <router-view class="layout"></router-view>
- <footer></footer>
+		<div class="seans"></div>
+	</header>
+
+	<router-view class="layout"></router-view>
+	<footer></footer>
 </template>
 
 <style scoped>
-.mainDiv{
-  display: grid;
-  width: 100%;
-  justify-content: center;
-  align-content: center;
-  height: 100%;
-  border-right: #98C15E solid 1px;
+.mainDiv {
+	display: grid;
+	width: 100%;
+	justify-content: center;
+	align-content: center;
+	height: 100%;
+	border-right: #98c15e solid 1px;
 }
-.layout{
-  margin: 0 auto;
+.layout {
+	margin: 0 auto;
 }
-.main{
-  font-size: 56px;
-  }
+.main {
+	font-size: 56px;
+}
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,76 +1,67 @@
 <script lang="ts">
-import { IFilms, IState}  from '../Types';
+import { IFilms, IState } from '../Types'
 
-let films:IFilms[];
-let states: IState[];
+let films: IFilms[]
+let states: IState[]
 
-export default{
-data(){
-    return {
-        films,
-        states
-    }
-},
+export default {
+	data() {
+		return {
+			films,
+			states,
+		}
+	},
 
-created(){
-    this.films =this.$cookies.get('films')
-    this.states =this.$cookies.get('states')
-}
-
+	created() {
+		this.films = this.$cookies.get('films')
+		this.states = this.$cookies.get('states')
+	},
 }
 </script>
 <template>
-
-<div class="container">
-
-    <template v-for="film in films">
-        <div v-if="film.idState == 1" class="film">
-            <img :src=film.image alt="film">
-            <div class="film_text">
-                <h1>{{ film.name }}</h1>
-            </div>
-        </div>
-    </template>
-    
-</div>
+	<div class="container">
+		<template v-for="film in films">
+			<div v-if="film.idState == 1" class="film">
+				<img :src="film.image" alt="film" />
+				<div class="film_text">
+					<h1>{{ film.name }}</h1>
+				</div>
+			</div>
+		</template>
+	</div>
 </template>
 <style scoped>
-
-.container{
-    display: grid;
-    width: 1256px;
-    border-radius: 15px;
-    border: #98C15E 1px solid;
-    background: #4B4B4B;
-    gap: 46px;
-    padding: 40px 31px;
+.container {
+	display: grid;
+	width: 1256px;
+	border-radius: 15px;
+	border: #98c15e 1px solid;
+	background: #4b4b4b;
+	gap: 46px;
+	padding: 40px 31px;
 }
 
-.film{
-    display: grid;
-    grid-template-columns: max-content 1fr;
-    background-color: #363636;
-    border-radius: 15px;
-    border: solid 1px #CDE735;
-    gap: 33px;
+.film {
+	display: grid;
+	grid-template-columns: max-content 1fr;
+	background-color: #363636;
+	border-radius: 15px;
+	border: solid 1px #cde735;
+	gap: 33px;
 }
 
-img{
-    border-radius: 15px;
-    border: #CDE735 solid 1px;
+img {
+	border-radius: 15px;
+	border: #cde735 solid 1px;
 }
 
-.film_text{
-    padding-top: 10px;
+.film_text {
+	padding-top: 10px;
 }
 
-h1{
-    font-size: 42px;
-    color: #CDE735;
-    -webkit-text-stroke: #363636 0.1px;
-    font-weight: bold;
+h1 {
+	font-size: 42px;
+	color: #cde735;
+	font-weight: bold;
 }
-
-
-
 </style>
